5dc222778181f82bb66be22535cc73485fdcb230
Eugene Musienko <zdravstvuy1714@gmail.com>
2025-02-27 17:34:01 +0600

    Enabled useful Laravel settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,30 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Password::defaults(callback: static fn(): Password => Password::min(size: 8)
+            ->letters()
+            ->mixedCase()
+            ->numbers()
+            ->symbols()
+            ->uncompromised());
+
+        Model::shouldBeStrict(shouldBeStrict: true);
+
+        Relation::enforceMorphMap(map: []);
+
+        Date::use(handler: CarbonImmutable::class);
+    }
 }
