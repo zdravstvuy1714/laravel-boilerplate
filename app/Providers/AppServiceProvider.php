9526387a7c3910defe0c19b59b6f153f351a3ad4
Eugene Musienko <zdravstvuy1714@gmail.com>
2025-02-27 17:54:28 +0600

    Fixed code style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        if (class_exists( \Laravel\Telescope\TelescopeServiceProvider::class) && $this->app->environment('local')) {
+        if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class) && $this->app->environment('local')) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
